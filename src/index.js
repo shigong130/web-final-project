356f834f635cfec24e97d5d4f4c15673aa0c0110
huizhifan <zhifanhui2017@gmail.com>
2020-04-24 11:40:52 -0700

    fix navbar

--- src/index.js
+++ src/index.js
@@ -16,7 +16,16 @@ import LoggedInComponent from './components/loggedin.component'
 import Register from "./containers/register.container";
 import MovieSearch from './containers/searchMovie.container'
 import MovieDetails from './containers/movieDetails.container'
-
+import {
+    Container,
+    Row,
+    Col,
+    Navbar,
+    Nav,
+    FormControl,
+    Form,
+    Button, NavbarBrand
+} from 'react-bootstrap';
 // import Register from "./containers/register.container";
 // import LoggedInComponent from './components/loggedin.component'
 
@@ -30,37 +39,18 @@ const movieSearchUrl = hostUrl + "/movie-search"
 ReactDOM.render(
     <Provider store={userStore}>
         <BrowserRouter>
-            <nav className="navbar navbar-expand-lg navbar-light bg-warning">
-                <a className="navbar-brand" href="home"><b>MovieReviews</b></a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href={loginUrl}>Login<span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href={registerUrl}>Register</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href={profileUrl}>Profile</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href={movieSearchUrl}>Search</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            {/*
-            <Link to={'/login'}> Login </Link>
-            <Link to={'/register'}> Register </Link>
-            <Link to={'/movie-search'}> Search Movie </Link>
-// <<<<<<< HEAD
-            */}
+            <Navbar collapseOnSelect expand="lg" bg="warning" variant="dark">
+                <Navbar.Brand style={{color:'black'}} href="home">MovieReviews</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link style={{color:'black'}} href={loginUrl}>Login</Nav.Link>
+                        <Nav.Link style={{color:'black'}} href={registerUrl}>Register</Nav.Link>
+                        <Nav.Link style={{color:'black'}} href={profileUrl}>Profile</Nav.Link>
+                        <Nav.Link style={{color:'black'}} href={movieSearchUrl}>Search</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
             <Switch>
                 {/*<Route path="/welcome" component={Welcome}/>*/}
                 <Route path="/home" component={Home}/>
