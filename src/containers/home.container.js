a4c13db4ad30bf4877130e30399efd692690a092
shigong130 <shigong130@gmail.com>
2020-04-24 02:56:34 -0700

    ui

--- src/containers/home.container.js
+++ src/containers/home.container.js
@@ -15,7 +15,7 @@ class Home extends React.Component {
                 <h3 class="text-center"><b>Welcome to Our Movie Review Website!</b></h3>
                 <div class="text-center">
                     <img class="img-fluid" src="https://www.midlothianlibrary.org/sitemedia/images/BooksandMedia/moviereview.jpg"
-                         alt="Responsive image" />
+                         alt="Responsive image" height="100" width="300"/>
                 </div>
                 </div>
 
