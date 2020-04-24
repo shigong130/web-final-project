489968a6e4894e425efcd28bd6f1bc380ab1a6f1
shigong130 <shigong130@gmail.com>
2020-04-24 02:39:55 -0700

    ui

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import MovieDetails from './containers/movieDetails.container'
 // import LoggedInComponent from './components/loggedin.component'
 
 const userStore = createStore(reducers, applyMiddleware(thunkMiddleware));
-const hostUrl = "http://localhost:3000"
+const hostUrl = window.location.href;
 const loginUrl = hostUrl + "/login"
 const registerUrl = hostUrl + "/register"
 const profileUrl = hostUrl + "/profile"
